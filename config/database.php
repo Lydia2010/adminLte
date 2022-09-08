2f805ca0d5049d076d6bd4ab1938aabd1c58f4f4
Lydia <lidiya.s@gmail.com>
2022-09-08 19:10:26 -0400

    DB

--- config/database.php
+++ config/database.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Str;
 
 $DATABASE_URL=parse_url('CLEARDB_DATABASE_URL');
+
+// $DATABASE_URL=parse_url('CLEARDB_DATABASE_URL');
 return [
 
     /*
@@ -47,11 +49,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('CLEARDB_DATABASE_URL'),
-            'host' => 'us-cdbr-east-06.cleardb.net',//$DATABASE_URL['host'],
-            'port' => '3306',//$DATABASE_URL['port'],
-            'database' => 'heroku_69fe2ffe17fb5b9',//ltrim($DATABASE_URL['path'], "/"),
-            'username' => 'baa5d2a47e610a', //$DATABASE_URL['user'],
-            'password' => '8e1932fd',//$DATABASE_URL['pass'],
+            'host' => $DATABASE_URL['host'],//env('DB_HOST', '127.0.0.1'),
+            'port' => $DATABASE_URL['port'],//env('DB_PORT', '3306'),
+            'database' => ltrim($DATABASE_URL['path'], '/'),//env('DB_DATABASE', 'forge'),
+            'username' => $DATABASE_URL['user'],//env('DB_USERNAME', 'forge'),
+            'password' => $DATABASE_URL['pass'],//env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -63,6 +65,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+
 
         'pgsql' => [
             'driver' => 'pgsql',
