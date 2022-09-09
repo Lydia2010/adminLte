ff91616f06078b0accc0c1be7212cde3fe41c797
Lydia <lidiya.s@gmail.com>
2022-09-08 20:57:37 -0400

    error message

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -44,20 +44,17 @@
                         </div>
                     </div>
                     <div class="input-group mb-3">
-                        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password">
+                        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" required autocomplete="new-password">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-lock"></span>
                             </div>
                         </div>
-
                         @error('password')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
                         @enderror
-
-
                     </div>
                     <div class="input-group mb-3">
                         <input type="password" name="password_confirmation" class="form-control" placeholder="Retype password">
